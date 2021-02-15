a0fcd86dd355c6c179babeb36c3d10a4b0eab29e
bezoui-software <64211294+bezoui-software@users.noreply.github.com>
2021-02-15 21:20:35 +0100

    Update switch.js

--- options/switch/switch.js
+++ options/switch/switch.js
@@ -1,9 +1,10 @@
-function createSwitch() {
+function createSwitch(state) {
   const toogle = document.createElement('label');
   toogle.classList.add('switch');
 
   const input = document.createElement('input'); 
   input.type = 'checkbox';
+  input.checked = state;
   toogle.appendChild(input);
 
   const slider = document.createElement('span');
